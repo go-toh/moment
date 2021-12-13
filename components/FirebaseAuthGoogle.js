9d46fe68299608e5d568d617edddaa9c07327858
go-toh <kurumiallergy@gmail.com>
2021-12-13 15:09:06 +0900

    FirebaseUIの廃止

--- components/FirebaseAuthGoogle.js
+++ components/FirebaseAuthGoogle.js
@@ -1,29 +1,37 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useRouter } from 'next/router';
+import { useSignInState } from '../contexts/SignInStateProvider';
+import GoogleIcon from '@mui/icons-material/Google';
 import Button from '@mui/material/Button';
 import initFirebase from '../src/firebaseConfig';
 
-const clickButton = () => {
-    
-    initFirebase();
+function GoogleSignInButton() {
+    const router = useRouter();
+    const { setUser } = useSignInState();
+    const clickButton = () => {
+        initFirebase();
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
 
-    const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = auth.currentUser;
+                const userState = {
+                    isSignIn: true,
+                    displayName: user.displayName,
+                    email: user.email,
+                    photoURL: user.photoURL,
+                }
+                setUser(userState);
+                router.push('/home');
+            }).catch((error) => {
+                console.error(error)
+            })
+    }
 
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            console.log(credential);
-            console.log(token);
-        }).catch((error) => {
-        console.error(error)
-        })
-}
-
-function GoogleLoginButton() {
     return (
-        <Button onClick={clickButton} variant="outlined">Googleログイン</Button>
+        <Button onClick={clickButton} variant="outlined" startIcon={<GoogleIcon />}>Googleサインイン</Button>
     );
 }
 
-export default GoogleLoginButton;
+export default GoogleSignInButton;
