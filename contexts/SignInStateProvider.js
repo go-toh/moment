373b2389eb8ba12bda565f5fbf3592370f761998
go-toh <kurumiallergy@gmail.com>
2021-12-17 15:07:54 +0900

    firestoreにユーザデータを作成

--- contexts/SignInStateProvider.js
+++ contexts/SignInStateProvider.js
@@ -1,6 +1,7 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "../src/firebaseConfig";
+import { isCreatedAccount, createAccount } from "../src/firebaseFirestore";
 
 const SignInStateContext = createContext();
 export const useSignInState = () => useContext(SignInStateContext);
@@ -12,11 +13,18 @@ function SignInStateProvider({ children }) {
         onAuthStateChanged(auth, (user) => {
             if(user) {
                 console.log("onAuthChanged signIn");
+
+                isCreatedAccount(user.uid).then((result) => {
+                    if(!result) createAccount(user);
+                })
+
                 setUserState({
                     displayName: user.displayName,
+                    uid: user.uid,
                     email: user.email,
                     photoURL: user.photoURL,
                 });
+                
             } else {
                 console.log("onAuthChanged not signIn");
                 setUserState();
---
+++ src/firebaseFirestore.js
@@ -0,0 +1,16 @@
+import { db } from "./firebaseConfig";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+
+export const isCreatedAccount = async(uid) => {
+    const userDocRef = doc(db, 'users', uid);
+    const docSnap = await getDoc(userDocRef);
+    return docSnap.exists();
+};
+
+export const createAccount = async(user) => {
+    await setDoc(doc(db, 'users', user.uid), {
+        displayName: user.displayName,
+        uid: user.uid,
+        email: user.email,
+    });
+};
