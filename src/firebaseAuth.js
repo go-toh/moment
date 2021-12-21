e0869fc1941c335eb1eaf1145ffdc3dd2cfd60e5
go-toh <kurumiallergy@gmail.com>
2021-12-21 10:02:10 +0900

    AuthをRedirectに変更

--- src/firebaseAuth.js
+++ src/firebaseAuth.js
@@ -1,9 +1,9 @@
 import { auth } from "./firebaseConfig";
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect } from 'firebase/auth'
 
 export const firebaseAuthSignInPopup = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
+    signInWithRedirect(auth, provider)
         .then((result) => {
             console.log("SignIn Success");
         }).catch((error) => {
