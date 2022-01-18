455357ef67b8facf49954c466be63cae3757c46c
go-toh <kurumiallergy@gmail.com>
2022-01-18 11:09:21 +0900

    providerで検索するのを変更

--- components/FilterSpotList.js
+++ components/FilterSpotList.js
@@ -3,10 +3,20 @@ import Spot from "./Spot";
 import Button from "@mui/material/Button";
 import { useSpotDataState } from "../contexts/SpotDataStateProvider";
 import { useNavigationBarState  } from "../contexts/NavigationBarStateProvider";
+import { useEffect, useState } from "react";
 
 function FilterSpotList() {
-    const { filterSpots } = useSpotDataState();
+    const { spots, filterConditions } = useSpotDataState();
     const { setNaviBarState } = useNavigationBarState ();
+    const [ filterSpots, setFilterSpots] = useState([]);
+
+    useEffect(() => {
+        setFilterSpots(spots.filter(spot =>
+            spot.spotArea == filterConditions.spotArea || 
+            spot.spotSeason == filterConditions.spotSeason ||
+            spot.spotTime == filterConditions.spotTime ||
+            spot.spotWeather == filterConditions.spotWeather))
+    }, [spots])
 
     const handleClick = () => {
         setNaviBarState("Search");
@@ -18,7 +28,6 @@ function FilterSpotList() {
                 {
                     filterSpots.map((spot, index)=> <Spot key={index}{...spot} />)
                 }
-                
             </Box>
             <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", mt: 2}}>
                 <Button onClick={handleClick} sx={{mb: 3}} variant="contained"  >
